ed466c8ccb268628c3dcfb69f26dedcf6023b64e
Joacovalle7 <joacovalle7@gmail.com>
2025-04-10 18:57:03 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <main>
+        <h2>Bienvenido a HardwareStore</h2>
+        <p>Tu tienda de confianza para componentes de PC 🖥️</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-logo">Vaie Hardware</h1>
+      <ul className="navbar-menu">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Ofertas</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">🛒 Carrito</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
